147b59f8b4c95c7dc8344c88028b570c922c53c9
Martin Pospisil <martin.pospisil@ideastatica.com>
2024-05-22 19:28:11 +0200

    adding BimApiCadClient

---
+++ src/templates/BimApiCadClient/BimApiLink/Importers/PlateImporter.cs
@@ -0,0 +1,50 @@
+﻿using BimApiCadClient.BimApi;
+using BimApiCadClient.CadApi;
+using IdeaStatiCa.BimApi;
+
+namespace BimApiCadClient.Importers
+{
+	internal class PlateImporter : BaseImporter<IIdeaPlate>
+	{
+		public PlateImporter(ICadGeometryApi model): base(model)
+		{
+		}
+
+		public override IIdeaPlate Create(int id)
+		{
+			var item = Model.GetPlate(id);
+			
+			if (item != null)
+			{
+				var p = new Plate(item.Id);
+				if (FillPlateInstance(item, p))
+				{
+					return p;
+				}
+				else
+				{
+					return null;
+				}
+			}
+
+			return null;
+		}
+
+		private bool FillPlateInstance(CadPlate cadplate, Plate plate) 
+		{
+			plate.MaterialNo = Model.GetMaterialByName(cadplate.Material).Id;
+
+			CadPlane3D plane = cadplate.CadOutline2D.Plane;
+
+			var localCoordinateSystem = plane.ToIdea();
+			
+			plate.LocalCoordinateSystem = localCoordinateSystem;
+
+			plate.Thickness = cadplate.Thickness;
+			plate.Geometry = cadplate.CadOutline2D.ToIdea();
+			plate.OriginNo = Utils.PointTranslator.GetPointId(cadplate.CadOutline2D.Plane.Origin);
+
+			return true;
+		}
+	}
+}
---
+++ src/templates/BimApiCadClient/CadApi/CadApi.cs
@@ -0,0 +1,12 @@
+﻿namespace BimApiCadClient.CadApi
+{
+	public interface ICadApi
+	{
+		ICadGeometryApi Geometry { get; }
+	}
+
+	public class CadApi : ICadApi
+	{
+		public ICadGeometryApi Geometry { get; } = new CadGeometryApi();
+	}
+}
