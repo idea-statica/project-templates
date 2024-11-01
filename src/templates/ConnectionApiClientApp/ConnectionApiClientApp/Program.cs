3046ca3322f77b00d343e21b4aa2341a43cab4e9
Martin Pospisil <martin.pospisil@ideastatica.com>
2024-11-01 12:18:37 +0100

    adding ConnectionApiClientApp

---
+++ src/templates/ConnectionApiClientApp/ConnectionApiClientApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using IdeaStatiCa.PluginLogger;
+using Newtonsoft.Json;
+using System.Reflection;
+
+namespace ConnectionApiClientApp
+{
+	internal class Program
+	{
+		static async Task Main(string[] args)
+		{
+			// initialize logger
+			string assemblyName = Assembly.GetExecutingAssembly().GetName().Name!;
+			var logfileName = Path.ChangeExtension(assemblyName, ".log");
+			SerilogFacade.Initialize(logfileName);
+
+			var logger = LoggerProvider.GetLogger(assemblyName);
+			{
+				try
+				{
+					// Get the directory of the executing assembly
+					string assemblyDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+
+					// Example usage of the directory
+					string ideaConFile = Path.Combine(assemblyDirectory, "Projects", "test1.ideaCon");
+
+					using (var calculator = new Calculator(logger))
+					{
+						logger.LogInformation("Starting CBFEM calculation");
+						var res = await calculator.CalculateAsync(ideaConFile, CancellationToken.None);
+						logger.LogInformation($"CBFEM calculation finished ");
+						// Convert the result to a JSON string
+						string jsonString = JsonConvert.SerializeObject(res, Formatting.Indented);
+						Console.WriteLine(jsonString);
+
+					}
+				}
+				catch (Exception ex)
+				{
+					logger.LogWarning("An error occurred", ex);
+				}
+			}
+		}
+	}
+}
